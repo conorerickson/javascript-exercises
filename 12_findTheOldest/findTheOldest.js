f4cb259a54563a53f31676843601b1779d21fa92
Conor Erickson <89137464+conorerickson@users.noreply.github.com>
2022-05-15 16:53:03 -0400

    Solved exercises 10 -12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  return array.reduce((oldest, person) => {
+    const oldestAge = age(oldest.birthYear, oldest.deathYear);
+    const currentAge = age(person.birthYear, person.deathYear);
+    return oldestAge < currentAge ? person : oldest;
+  })
 };
+const age = function(birth, death) {
+  if(!death) {
+    death = new Date().getFullYear;
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
